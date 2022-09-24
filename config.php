c294f4b08277e7d1b3c52d6bf8c31d3698a70d36
BLESSING29 <atchrimiblessing@gmail.com>
2022-09-24 12:04:11 +0100

    nouveau mise a jour dbb

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 
   try 
   {
-      $bdd = new PDO("mysql:host=localhost;dbname=e-learning_wise;charset=utf8",'root', 'Blessing1999@');
+      $bdd = new PDO("mysql:host=ftp-e-learningmanasse.alwaysdata.net;dbname=e-learning_wise;charset=utf8",'e-learningmanasse', 'Blessing1999@');
   }
   catch(PDOException $e)
   {
--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php 
     try{
-        $bdd = new PDO('mysql:host=localhost;dbname=e-learning_wise;charset=utf8', 'root', 'Blessing1999@');
+        $bdd = new PDO('mysql:host=ftp-e-learningmanasse.alwaysdata.net;dbname=e-learning_wise;charset=utf8', 'e-learningmanasse', 'Blessing1999@');
     }catch(\Exception $e){
         die('Erreur : '.$e->getMessage());
       }
